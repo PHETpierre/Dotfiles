c112721ca8ccdc7f67a5ec2f33f7dc7ce48f33a7
pierre <phpierre@hotmail.fr>
2023-04-29 21:56:54 +0200

    Adding files.
     Changes to be committed:
    	new file:   .alacritty.yml
    	new file:   .bashrc
    	new file:   i3/config
    	new file:   mkLink.sh
    	new file:   rc.conf

---
+++ mkLink.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#home
+ln -sf /home/pierre/.dotfiles/.bashrc /home/pierre/.bashrc
+ln -sf /home/pierre/.dotfiles/.alacritty.yml /home/pierre/.alacritty.yml
+ln -sf /home/pierre/.dotfiles/i3/config /home/pierre/.config/i3/config
+
+#etc
+ln -sf /home/pierre/.dotfiles/rc.conf /etc/ranger/config/rc.conf 
+
+# $SHELL
