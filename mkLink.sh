e9f752f4edd066f6a24d912c2acb657038367491
PHETpierre <phpierre@hotmail.fr>
2023-06-21 15:24:12 +0200

    Changes to be committed: 	new file:   .agignore 	modified:   mkLink.sh

--- mkLink.sh
+++ mkLink.sh
@@ -5,6 +5,7 @@ ln -sf /home/pierre/.dotfiles/.bashrc /home/pierre/.bashrc
 ln -sf /home/pierre/.dotfiles/.alacritty.yml /home/pierre/.alacritty.yml
 ln -sf /home/pierre/.dotfiles/i3/config /home/pierre/.config/i3/config
 ln -sf /home/pierre/.dotfiles/.zshrc /home/pierre/.zshrc
+ln -sf /home/pierre/.dotfiles/.agignore /home/pierre/.agignore
 
 ln -sf /home/pierre/.dotfiles/nvim/init.vim /home/pierre/.config/nvim/init.vim
 ln -sf /home/pierre/.dotfiles/nvim/lua/plugins.lua /home/pierre/.config/nvim/lua/plugins.lua
