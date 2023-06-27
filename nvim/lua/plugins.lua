03b59d0376fa30fb369ad60b21f7c911b3852034
PHETpierre <phpierre@hotmail.fr>
2023-06-27 15:30:15 +0200

    Changes to be committed: 	modified:   nvim/lua/plugins.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -68,7 +68,10 @@ return require('packer').startup(function(use)
   }
 
   -- You can specify multiple plugins in a single call
-  use {'tjdevries/colorbuddy.vim', {'nvim-treesitter/nvim-treesitter', opt = true}}
+  use { 
+      'tjdevries/colorbuddy.vim', 
+      { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  }
 
   -- You can alias plugin names
   -- use {'dracula/vim', as = 'dracula'}
